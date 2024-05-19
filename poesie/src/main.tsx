6a170c8e10e0ac56f0c0f4e18db5ac6c04ca1cf4
DryValley <michelangelo.valsecchi@gmail.com>
2024-05-19 11:45:07 +0100

    Added mantine

--- poesie/src/main.tsx
+++ poesie/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './app.tsx'
 import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <MantineProvider defaultColorScheme='dark'>
+            <App />
+        </MantineProvider>
+    </React.StrictMode>,
 )
